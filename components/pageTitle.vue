af6c47796386753b10d1cd9a3b93ab9e701f674c
JuliusDeng <1576554007@qq.com>
2021-02-23 23:24:02 +0800

    开发页title组件。

---
+++ components/pageTitle.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="flex a-center">
+			<my-icon iconId="icon-jiantou-copy" class="mx-2"  @my-click="quit"></my-icon>
+			<view class="font-lg">
+				<slot></slot>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			quit() {
+				console.log('1111');
+				uni.navigateBack({
+					delta: 1
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/readList/readList.vue
+++ pages/readList/readList.vue
@@ -1,14 +1,20 @@
 <template>
 	<view>
-		sdfssdfsd
+		<!-- 顶部导航高度占位 -->
+		<view :style="{height: `${statusBarHeight}px`}"></view>
+		<page-title>榜单</page-title>
 	</view>
 </template>
 
 <script>
+	import pageTitle from '@/components/pageTitle.vue';
 	export default {
+		components:{
+			pageTitle
+		},
 		data() {
 			return {
-				
+				statusBarHeight: this.$statusBarHeight
 			}
 		},
 		methods: {
